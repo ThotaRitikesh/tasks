3ea13e9cdb0636ae40b7911912a6d6cce13d5364
ThotaRitikesh <thotaritikesh@gmail.com>
2023-04-14 00:13:22 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,124 +1,119 @@
-import { useEffect, useState, useRef } from "react";
-import "./App.css";
+import React from 'react'
+import './App.css'
 
 function App() {
-  const timerRef = useRef();
-  const inputRef = useRef();
-  const [currentTime, setCurrentTime] = useState(0);
-  const [selectedUnit, setSelectedUnit] = useState("seconds");
-  const [timerRunning, setTimerRunning] = useState(false);
-
-  useEffect(() => {
-    return () => clearInterval(timerRef.current);
-  }, []);
-
-  const startTimer = () => {
-    setTimerRunning(true);
-    timerRef.current = setInterval(() => {
-      setCurrentTime((prev) => prev - 1);
-    }, 1000);
-  };
-
-  useEffect(() => {
-    if (currentTime === 0) {
-      clearInterval(timerRef.current);
-      setTimerRunning(false);
-      inputRef.current.value = "";
-    }
-  }, [currentTime]);
-
-  const handleStart = (e) => {
-    e.preventDefault();
-
-    if (timerRef.current) clearInterval(timerRef.current);
-    const timeInput = inputRef.current.value;
-
-    // const timeInput = parseInt(document.getElementById("timeInput").textContent);
-    const convertedTime =
-      selectedUnit === "hours"
-        ? timeInput * 3600
-        : selectedUnit === "minutes"
-        ? timeInput * 60
-        : timeInput;
-    setCurrentTime(convertedTime);
-    startTimer();
-  };
-
-  const handleReset = () => {
-    clearInterval(timerRef.current);
-    setCurrentTime(0);
-    document.getElementById("timeInput").textContent = "";
-    setTimerRunning(false);
-    inputRef.current.value = "";
-    setIsHidden(true);
-  };
-
-  const unitOptions = ["seconds", "minutes", "hours"];
-
-  const formatTime = (time) => {
-    const date = new Date(time * 1000);
-    const hours = date.getUTCHours().toString().padStart(2, "0");
-    const minutes = date.getUTCMinutes().toString().padStart(2, "0");
-    const seconds = date.getUTCSeconds().toString().padStart(2, "0");
-    return `${hours}:${minutes}:${seconds}`;
-  };
-  const [isHidden, setIsHidden] = useState(true);
-
-  // const handleClick1 = () => {
-  //   setIsHidden(true);
-  // };
-
-  const parent = () =>{
-    // document.getElementById("unitOptions").classList.toggle("show")
-    setIsHidden(false);
-  }
-
-  const handleSelect = (e) => {
-    setSelectedUnit(e.target.textContent);
-    setIsHidden(true);
-  }
-
   return (
-    <div className="App">
-      <div className="header">
-        <button type="reset" onClick={handleReset}>
-        <span class="material-symbols-outlined">restart_alt</span>
-        </button>
-      </div>
-      <div className="middle">
-        <div contentEditable={timerRunning ? false : true} id="timeInput" className="inputDiv"></div>
-        <div>
-        <div className="selectDiv" disabled={timerRunning}
-        onClick={parent}
-        // onClick={() =>document.getElementById("unitOptions").classList.toggle("show")}
-        >
-          {selectedUnit}
-        </div>
-        {!isHidden && (
-          <div id="unitOptions" className="optionsDiv" >
-            {unitOptions.map((option) => (
-              <div key={option} onClick={handleSelect}>
-                {option}
-              </div>
-            ))}
-          </div>
-           )}
-           </div>
-        <form onSubmit={handleStart}>
-           <input
-            type="number"
-            autoFocus
-            placeholder={`Enter total ${selectedUnit} to countdown`}
-            ref={inputRef}
-            disabled={timerRunning}
-          />
-          </form>
-      </div>
-      <div className="time">
-      {formatTime(currentTime)}
-      </div>
-    </div>
-  );
+    <div className="table">
+  <div className="row header">
+    <div className="cell">Header 1</div>
+    <div className="cell">Header 2</div>
+    <div className="cell">Header 3</div>
+  </div>
+  <div className="scrollable">
+  <div className="row">
+    <div className="cell">Row 1, Cell 1</div>
+    <div className="cell">Row 1, Cell 2</div>
+    <div className="cell">Row 1, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 2, Cell 1</div>
+    <div className="cell">Row 2, Cell 2</div>
+    <div className="cell">Row 2, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 3, Cell 1</div>
+    <div className="cell">Row 3, Cell 2</div>
+    <div className="cell">Row 3, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 4, Cell 1</div>
+    <div className="cell">Row 4, Cell 2</div>
+    <div className="cell">Row 4, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 5, Cell 1</div>
+    <div className="cell">Row 5, Cell 2</div>
+    <div className="cell">Row 5, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 6, Cell 1</div>
+    <div className="cell">Row 6, Cell 2</div>
+    <div className="cell">Row 6, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 7, Cell 1</div>
+    <div className="cell">Row 7, Cell 2</div>
+    <div className="cell">Row 7, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 8, Cell 1</div>
+    <div className="cell">Row 8, Cell 2</div>
+    <div className="cell">Row 8, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 9, Cell 1</div>
+    <div className="cell">Row 9, Cell 2</div>
+    <div className="cell">Row 9, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 10, Cell 1</div>
+    <div className="cell">Row 10, Cell 2</div>
+    <div className="cell">Row 10, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 11, Cell 1</div>
+    <div className="cell">Row 11, Cell 2</div>
+    <div className="cell">Row 11, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 12, Cell 1</div>
+    <div className="cell">Row 12, Cell 2</div>
+    <div className="cell">Row 12, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 13, Cell 1</div>
+    <div className="cell">Row 13, Cell 2</div>
+    <div className="cell">Row 13, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 14, Cell 1</div>
+    <div className="cell">Row 14, Cell 2</div>
+    <div className="cell">Row 14, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 15, Cell 1</div>
+    <div className="cell">Row 15, Cell 2</div>
+    <div className="cell">Row 15, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 16, Cell 1</div>
+    <div className="cell">Row 16, Cell 2</div>
+    <div className="cell">Row 16, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 17, Cell 1</div>
+    <div className="cell">Row 17, Cell 2</div>
+    <div className="cell">Row 17, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 18, Cell 1</div>
+    <div className="cell">Row 18, Cell 2</div>
+    <div className="cell">Row 18, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 19, Cell 1</div>
+    <div className="cell">Row 19, Cell 2</div>
+    <div className="cell">Row 19, Cell 3</div>
+  </div>
+  <div className="row">
+    <div className="cell">Row 20, Cell 1</div>
+    <div className="cell">Row 20, Cell 2</div>
+    <div className="cell">Row 20, Cell 3</div>
+  </div>
+</div>
+</div>
+
+  )
 }
 
-export default App;
+export default App
