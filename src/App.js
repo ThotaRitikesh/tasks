e93ce063b0ec03b46422a6ea4b453a872bc7ad67
ThotaRitikesh <thotaritikesh@gmail.com>
2023-04-14 13:10:01 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,118 +1,189 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TextField from '@material-ui/core/TextField';
+// import Dialog from '@material-ui/core/Dialog';
+// import DialogTitle from '@material-ui/core/DialogTitle';
+// import DialogContent from '@material-ui/core/DialogContent';
+// import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogActions from '@material-ui/core/DialogActions';
+// import Button from '@material-ui/core/Button';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Checkbox from '@material-ui/core/Checkbox';
+import Autocomplete 
+// { createFilterOptions } 
+from '@material-ui/lab/Autocomplete';
+import { top100Films } from './autoCompleteData'
 import './App.css'
 
-function App() {
-  return (
-    <div className="table">
-  <div className="row header">
-    <div className="cell">Header 1</div>
-    <div className="cell">Header 2</div>
-    <div className="cell">Header 3</div>
-  </div>
-  <div className="scrollable">
-  <div className="row">
-    <div className="cell">Row 1, Cell 1</div>
-    <div className="cell">Row 1, Cell 2</div>
-    <div className="cell">Row 1, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 2, Cell 1</div>
-    <div className="cell">Row 2, Cell 2</div>
-    <div className="cell">Row 2, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 3, Cell 1</div>
-    <div className="cell">Row 3, Cell 2</div>
-    <div className="cell">Row 3, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 4, Cell 1</div>
-    <div className="cell">Row 4, Cell 2</div>
-    <div className="cell">Row 4, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 5, Cell 1</div>
-    <div className="cell">Row 5, Cell 2</div>
-    <div className="cell">Row 5, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 6, Cell 1</div>
-    <div className="cell">Row 6, Cell 2</div>
-    <div className="cell">Row 6, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 7, Cell 1</div>
-    <div className="cell">Row 7, Cell 2</div>
-    <div className="cell">Row 7, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 8, Cell 1</div>
-    <div className="cell">Row 8, Cell 2</div>
-    <div className="cell">Row 8, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 9, Cell 1</div>
-    <div className="cell">Row 9, Cell 2</div>
-    <div className="cell">Row 9, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 10, Cell 1</div>
-    <div className="cell">Row 10, Cell 2</div>
-    <div className="cell">Row 10, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 11, Cell 1</div>
-    <div className="cell">Row 11, Cell 2</div>
-    <div className="cell">Row 11, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 12, Cell 1</div>
-    <div className="cell">Row 12, Cell 2</div>
-    <div className="cell">Row 12, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 13, Cell 1</div>
-    <div className="cell">Row 13, Cell 2</div>
-    <div className="cell">Row 13, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 14, Cell 1</div>
-    <div className="cell">Row 14, Cell 2</div>
-    <div className="cell">Row 14, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 15, Cell 1</div>
-    <div className="cell">Row 15, Cell 2</div>
-    <div className="cell">Row 15, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 16, Cell 1</div>
-    <div className="cell">Row 16, Cell 2</div>
-    <div className="cell">Row 16, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 17, Cell 1</div>
-    <div className="cell">Row 17, Cell 2</div>
-    <div className="cell">Row 17, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 18, Cell 1</div>
-    <div className="cell">Row 18, Cell 2</div>
-    <div className="cell">Row 18, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 19, Cell 1</div>
-    <div className="cell">Row 19, Cell 2</div>
-    <div className="cell">Row 19, Cell 3</div>
-  </div>
-  <div className="row">
-    <div className="cell">Row 20, Cell 1</div>
-    <div className="cell">Row 20, Cell 2</div>
-    <div className="cell">Row 20, Cell 3</div>
-  </div>
-</div>
-</div>
+// const filter = createFilterOptions();
 
+function App() {
+  // const [value, setValue] = useState(null);
+  const [state, setState] = useState({
+    gilad: true,
+    jason: false,
+    antoine: false,
+  });
+  // const [open, toggleOpen] = React.useState(false);
+  // const handleClose = () => {
+  //   setDialogValue({
+  //     title: '',
+  //     year: '',
+  //   });
+
+  //   toggleOpen(false);
+  // };
+
+  // const [dialogValue, setDialogValue] = React.useState({
+  //   title: '',
+  //   year: '',
+  // });
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   setValue({
+  //     title: dialogValue.title,
+  //     year: parseInt(dialogValue.year, 10),
+  //   });
+
+  //   handleClose();
+  // };
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+  const { gilad, jason, antoine } = state;
+  return (
+<div className='container'>
+  <div>
+  <form  noValidate autoComplete="off">
+  <TextField id="outlined-basic" label="Outlined" variant="outlined" className='txt'/> <br />
+  <TextField id="outlined-basic" label="Outlined" variant="outlined" className='txt'/> <br />
+  <TextField id="outlined-basic" label="Outlined" variant="outlined" className='txt'/> <br />
+  <TextField id="outlined-basic" label="Outlined" variant="outlined" className='txt'/> <br />
+  </form>
+  </div>
+  {/* <Autocomplete
+        value={value}
+        // onChange={(event, newValue) => {
+        //   if (typeof newValue === 'string') {
+        //     // timeout to avoid instant validation of the dialog's form.
+        //     setTimeout(() => {
+        //       // toggleOpen(true);
+        //       setDialogValue({
+        //         title: newValue,
+        //         year: '',
+        //       });
+        //     });
+        //   } else if (newValue && newValue.inputValue) {
+        //     toggleOpen(true);
+        //     setDialogValue({
+        //       title: newValue.inputValue,
+        //       year: '',
+        //     });
+        //   } else {
+        //     setValue(newValue);
+        //   }
+        // }}
+        filterOptions={(options, params) => {
+          const filtered = filter(options, params);
+
+          if (params.inputValue !== '') {
+            filtered.push({
+              inputValue: params.inputValue,
+              title: `Add "${params.inputValue}"`,
+            });
+          }
+
+          return filtered;
+        }}
+        id="free-solo-dialog-demo"
+        options={top100Films}
+        getOptionLabel={(option) => {
+          // e.g value selected with enter, right from the input
+          if (typeof option === 'string') {
+            return option;
+          }
+          if (option.inputValue) {
+            return option.inputValue;
+          }
+          return option.title;
+        }}
+        selectOnFocus
+        clearOnBlur
+        handleHomeEndKeys
+        renderOption={(option) => option.title}
+        style={{ width: 300 }}
+        freeSolo
+        renderInput={(params) => (
+          <TextField {...params} label="Free solo dialog" variant="outlined" />
+        )}
+      /> */}
+      <div style={{width: '250px' }}>
+       <Autocomplete
+        id="free-solo-demo"
+        freeSolo
+        options={top100Films.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField {...params} label="freeSolo" margin="normal" variant="outlined" />
+        )}
+      />
+             <Autocomplete
+        id="free-solo-demo"
+        freeSolo
+        options={top100Films.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField {...params} label="freeSolo" margin="normal" variant="outlined" />
+        )}
+      />
+             <Autocomplete
+        id="free-solo-demo"
+        freeSolo
+        options={top100Films.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField {...params} label="freeSolo" margin="normal" variant="outlined" />
+        )}
+      />
+             <Autocomplete
+        id="free-solo-demo"
+        freeSolo
+        options={top100Films.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField {...params} label="freeSolo" margin="normal" variant="outlined" />
+        )}
+      />
+      </div>
+      <div>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Assign responsibility</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={gilad} onChange={handleChange} name="gilad" />}
+            label="Gilad Gray"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={jason} onChange={handleChange} name="jason" />}
+            label="Jason Killian"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={antoine} onChange={handleChange} name="antoine" />}
+            label="Antoine Llorca"
+          />
+        </FormGroup>
+        <FormHelperText>Be careful</FormHelperText>
+      </FormControl>
+      </div>
+      <div>
+  <form  noValidate autoComplete="off">
+  <TextField id="outlined-basic" label="Outlined" variant="outlined" className='txt'/> <br /> <br />
+  <TextField id="outlined-basic" label="Outlined" variant="outlined" className='txt'/> <br /> <br />
+  <TextField id="outlined-basic" label="Outlined" variant="outlined" className='txt'/> <br />
+  <TextField id="outlined-basic" label="Outlined" variant="outlined" className='txt'/> <br />
+  </form>
+  </div>
+</div>
   )
 }
 
