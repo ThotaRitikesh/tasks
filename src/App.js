c5867781201bb3d041f4e4882d0b9f72b82f42ce
ThotaRitikesh <thotaritikesh@gmail.com>
2023-04-14 22:37:00 +0530

    task3 completed

--- src/App.js
+++ src/App.js
@@ -8,50 +8,56 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import Checkbox from '@material-ui/core/Checkbox';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { top100Films } from './autoCompleteData';
+import { customerData } from './data1';
 import './App.css';
 
 function App() {
-  const [fields, setFields] = useState([
-    { label: 'First Name', value: 'Thota' },
-    { label: 'Last Name', value: 'Ritikesh' },
-    { label: 'Address', value: 'Hyderabad' },
-    { label: 'State', value: 'Telangana' },
-    { label :'Customer-Id', value:'12345'},
-    { label :'phone number', value:'98765433456'},
-    { label :'Age', value:'20'}
-  ]);
 
+  const [fields, setFields] = useState(customerData);
   const [fields1, setFields1] = useState([
-    { label: 'movie 1', value: 'The Godfather' },
-    { label: 'movie 2', value: 'The Matrix' },
-    // { label: 'Field 3', value: 'test2' },
-    // { label: 'Field 4', value: 'test3' },
-  ]);
+  { label: 'movie 1', value: 'The Godfather' },
+  { label: 'movie 2', value: 'The Matrix' },
+]);
+const [fields2, setFields2] = useState([
+  { label: 'placed order', value: 'yes' },
+  { label: 'success', value: '' },
+]);
 
-  const [fields2, setFields2] = useState([
-    { label: 'placed order', value: 'yes' },
-    { label: 'success', value: '' },
-    // { label: 'Field 3', value: 'test2' },
-    // { label: 'Field 4', value: 'test3' },
-  ]);
+const handleFieldChange = (index) => (event) => {
+  const newFields = [...fields];
+  newFields[index].value = event.target.value;
+  setFields(newFields);
+};
 
+const handleAutocompleteChange = (index) => (_, value) => {
+  const newFields1 = [...fields1];
+  newFields1[index].value = value || '';
+  setFields1(newFields1);
+};
+const fieldsObj = {};
+fields.forEach(field => {
+  fieldsObj[field.label] = field.value;
+});
 
-  const handleFieldChange = (index) => (event) => {
-    const newFields = [...fields];
-    newFields[index].value = event.target.value;
-    setFields(newFields);
-  };
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const data = {};
+  
+  fields.forEach(field => {
+    data[field.label] = field.value;
+  });
+  
+  fields1.forEach((field, index) => {
+    data[`Autocomplete ${index}`] = field.value;
+  });
+  
+  fields2.forEach((field, index) => {
+    data[field.label] = field.value !== '' ? 'yes' : 'no';
+  });
+  
+  console.log(JSON.stringify(data, null, 2));
+};
 
-  const handleAutocompleteChange = (index) => (_, value) => {
-    const newFields1 = [...fields1];
-    newFields1[index].value = value || '';
-    setFields1(newFields1);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log({...fields,...fields1});
-  };
 
   return (
     <div className='container'>
@@ -73,7 +79,7 @@ function App() {
               id={`autocomplete-${index}`}
               options={top100Films.map((option) => option.title)}
               onChange={handleAutocompleteChange(index)}
-              // defaultValue={field.value}
+              defaultValue={field.value}
               renderInput={(params) => (
                 <TextField
                   {...params}
@@ -106,88 +112,3 @@ function App() {
 }
 
 export default App;
-
-// import React, { useState } from 'react'
-// import TextField from '@material-ui/core/TextField';
-// import FormLabel from '@material-ui/core/FormLabel';
-// import FormControl from '@material-ui/core/FormControl';
-// import FormGroup from '@material-ui/core/FormGroup';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import FormHelperText from '@material-ui/core/FormHelperText';
-// import Checkbox from '@material-ui/core/Checkbox';
-// import Autocomplete from '@material-ui/lab/Autocomplete';
-// import { top100Films } from './autoCompleteData'
-// import './App.css'
-
-// function App() {
-//   const [fields, setFields] = useState([
-//     { label: 'Field 1', value: 'test1' },
-//     { label: 'Field 2', value: 'test2' },
-//     { label: 'Field 3', value: '' },
-//     { label: 'Field 4', value: '' },
-//   ]);
-
-//   const handleFieldChange = (index) => (event) => {
-//     const newFields = [...fields];
-//     newFields[index].value = event.target.value;
-//     setFields(newFields);
-//     console.log(fields);
-//   };
-
-//   return (
-//     <div className='container'>
-//       <div>
-//         <form noValidate autoComplete="off">
-//           {fields.map((field, index) => (
-//             <><TextField
-//               key={index}
-//               id={`field-${index}`}
-//               label={field.label}
-//               variant="outlined"
-//               className='txt'
-//               value={field.value}
-//               onChange={handleFieldChange(index)}
-//             /><br /></>
-//           ))}
-//         </form>
-//       </div>
-//       <div style={{ width: '250px' }}>
-//         {/* Map over the fields array to create Autocomplete components */}
-//         {fields.map((field, index) => (
-//           <Autocomplete
-//             key={index}
-//             id={`autocomplete-${index}`}
-//             freeSolo
-//             options={top100Films.map((option) => option.title)}
-//             renderInput={(params) => (
-//               <TextField
-//                 {...params}
-//                 label={`Autocomplete ${index}`}
-//                 margin="normal"
-//                 variant="outlined"
-//               />
-//             )}
-//           />
-//         ))}
-//       </div>
-//       <div>
-//         <FormControl component="fieldset">
-//           <FormLabel component="legend">Assign responsibility</FormLabel>
-//           <FormGroup>
-//             {fields.map((field, index) => (
-//               <FormControlLabel
-//                 key={index}
-//                 control={<Checkbox checked={field.value} onChange={handleFieldChange(index)} name={`field-${index}`} />}
-//                 label={field.label}
-//               />
-//             ))}
-//           </FormGroup>
-//           <FormHelperText>Be careful</FormHelperText>
-//         </FormControl>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App;
-
---
+++ src/data1.js
@@ -0,0 +1,9 @@
+export const customerData = [
+  { label: "First Name", value: "Thota" },
+  { label: "Last Name", value: "Ritikesh" },
+  { label: "Address", value: "Hyderabad" },
+  { label: "State", value: "Telangana" },
+  { label: "Customer-Id", value: "12345" },
+  { label: "phone number", value: "98765433456" },
+  { label: "Age", value: "20" },
+];
