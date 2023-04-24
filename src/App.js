18bbf0bf51b3e9e1958e9b6e02b09197fc41b685
ThotaRitikesh <thotaritikesh@gmail.com>
2023-04-24 14:25:36 +0530

    done with tic

--- src/App.js
+++ src/App.js
@@ -1,117 +1,77 @@
-// import { useState } from "react";
-// import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 
-// function App() {
-//   const [selectedUnit, setSelectedUnit] = useState("Select options");
-//   const unitOptions = ["option 1", "option 2", "option 3", "option 4","option 5","option 6"];
-//   const [isHidden, setIsHidden] = useState(true);
+const App = () => {
+  const [grid, setGrid] = useState(Array(9).fill(null)); // grid state to store the grid items
+  const [currentPlayer, setCurrentPlayer] = useState('red'); // state to track the current player
+  const [round, setRound] = useState(1); // state to track the current round
 
-//   const parent = () =>{
-//     setIsHidden(false);
-//   }
+  const handleItemClick = (index) => {
+    if (grid[index] === null) {
+      // check if the grid item is not already filled
+      const updatedGrid = [...grid];
+      updatedGrid[index] = currentPlayer; // fill the grid item with current player's color
+      setGrid(updatedGrid);
 
-//   const handleSelect = (e) => {
-//     setSelectedUnit(e.target.textContent);
-//     setIsHidden(true);
-//   }
-
-//   return (
-//     <div className="App">
-//       <div className="middle">
-//         <div>
-//           <div className="selectDiv" onClick={parent}>
-//             {selectedUnit}
-//           </div>
-//           {!isHidden && (
-//             <div id="unitOptions" className="optionsDiv">
-//               {unitOptions.map((option) => (
-//                 <div key={option} onClick={handleSelect}>
-//                   {option}
-//                 </div>
-//               ))}
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import { useState } from "react";
-import Button from '@mui/material/Button';
-import Alert from "./Alert";
-import "./App.css";
-
-function App() {
-  const [selectedOptions, setselectedOptions] = useState([]);
-  const unitOptions = ["option 1", "option 2", "option 3", "option 4", "option 5", "option 6"];
-  const [isHidden, setIsHidden] = useState(true);
-  const [open, setOpen] = useState(false);
-
-  const handleClose = () => {
-    setOpen(false);
+      if (checkWin(updatedGrid)) {
+        // check if current player wins
+        alert(`${currentPlayer.toUpperCase()} wins!`);
+        resetGame();
+      } else if (round === 9) {
+        // check if it's a draw
+        alert("It's a draw!");
+        resetGame();
+      } else {
+        setCurrentPlayer(currentPlayer === 'red' ? 'blue' : 'red'); // switch to the other player
+      }
+    }
   };
 
-  const parent = () => {
-    setIsHidden(false);
-  }
+  const checkWin = (grid) => {
+    // array to store all possible winning combinations
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
 
-  const handleSelect = (e) => {
-    const option = e.target.textContent;
-    if (!selectedOptions.includes(option)) {
-      setselectedOptions([...selectedOptions, option]);
-    }else {
-      // alert("Option already selected!");
-      setOpen(true);
-
+    // loop through all winning combinations and check if any player has 3 consecutive grid items with the same color
+    for (let i = 0; i < winningCombinations.length; i++) {
+      const [a, b, c] = winningCombinations[i];
+      if (grid[a] && grid[a] === grid[b] && grid[a] === grid[c]) {
+        return true;
+      }
     }
-  }
+    return false;
+  };
 
-  // const handleRemove = (option) => {
-  //   const updatedUnits = selectedOptions.filter(unit => unit !== option);
-  //   setselectedOptions(updatedUnits);
-  // }
-  const handleDone = ()=>{
-    setIsHidden(true);
-  }
-
+  const resetGame = () => {
+    setGrid(Array(9).fill(null)); // reset grid
+    setCurrentPlayer('red'); // reset current player
+    setRound(round + 1); // increment round
+  };
 
   return (
-    <div className="App">
-      <div className="middle">
-        <div>
-          <div className="selectDiv" onClick={parent}>
-            {selectedOptions.length > 0 ? selectedOptions.join(", ") : "Select options"}
+    <div className="container">
+      <h1>Round: {round}</h1>
+      <div className="grid">
+        {grid.map((item, index) => (
+          <div
+            key={index}
+            className={`grid-item ${item}`}
+            onClick={() => handleItemClick(index)}
+          >
+            {item}
           </div>
-          {!isHidden && (
-            <div id="unitOptions" className="optionsDiv">
-              {unitOptions.map((option) => (
-                <div key={option} onClick={handleSelect} className="options">
-                  {option}
-                </div>
-              ))}
-            </div>
-          )}
-          <Button variant="outlined" onClick={handleDone} style={{marginTop:"20px"}}>Save</Button>
-          <Alert open={open} handleClose={handleClose}/>
-        </div>
+        ))}
       </div>
-      {/* <div>
-        {selectedOptions.length > 0 && (
-          <div className="selectedDiv">
-            <div>Selected options:</div>
-            {selectedOptions.map((option) => (
-              <div key={option} onClick={() => handleRemove(option)}>
-                {option}
-              </div>
-            ))}
-          </div>
-        )}
-      </div> */}
     </div>
   );
-}
+};
 
 export default App;
