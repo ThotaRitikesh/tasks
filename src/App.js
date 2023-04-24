bedac23c56774320faa02b466bcd2bd04e651f03
ThotaRitikesh <thotaritikesh@gmail.com>
2023-04-24 17:42:56 +0530

    dropdown wip

--- src/App.js
+++ src/App.js
@@ -1,117 +1,13 @@
-// import { useState } from "react";
-// import "./App.css";
-
-// function App() {
-//   const [selectedUnit, setSelectedUnit] = useState("Select options");
-//   const unitOptions = ["option 1", "option 2", "option 3", "option 4","option 5","option 6"];
-//   const [isHidden, setIsHidden] = useState(true);
-
-//   const parent = () =>{
-//     setIsHidden(false);
-//   }
-
-//   const handleSelect = (e) => {
-//     setSelectedUnit(e.target.textContent);
-//     setIsHidden(true);
-//   }
-
-//   return (
-//     <div className="App">
-//       <div className="middle">
-//         <div>
-//           <div className="selectDiv" onClick={parent}>
-//             {selectedUnit}
-//           </div>
-//           {!isHidden && (
-//             <div id="unitOptions" className="optionsDiv">
-//               {unitOptions.map((option) => (
-//                 <div key={option} onClick={handleSelect}>
-//                   {option}
-//                 </div>
-//               ))}
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import { useState } from "react";
-import Button from '@mui/material/Button';
-import Alert from "./Alert";
-import "./App.css";
+import React from 'react'
+import Select from './Select'
 
 function App() {
-  const [selectedOptions, setselectedOptions] = useState([]);
-  const unitOptions = ["option 1", "option 2", "option 3", "option 4", "option 5", "option 6"];
-  const [isHidden, setIsHidden] = useState(true);
-  const [open, setOpen] = useState(false);
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const parent = () => {
-    setIsHidden(false);
-  }
-
-  const handleSelect = (e) => {
-    const option = e.target.textContent;
-    if (!selectedOptions.includes(option)) {
-      setselectedOptions([...selectedOptions, option]);
-    }else {
-      // alert("Option already selected!");
-      setOpen(true);
-
-    }
-  }
-
-  // const handleRemove = (option) => {
-  //   const updatedUnits = selectedOptions.filter(unit => unit !== option);
-  //   setselectedOptions(updatedUnits);
-  // }
-  const handleDone = ()=>{
-    setIsHidden(true);
-  }
-
-
   return (
-    <div className="App">
-      <div className="middle">
-        <div>
-          <div className="selectDiv" onClick={parent}>
-            {selectedOptions.length > 0 ? selectedOptions.join(", ") : "Select options"}
-          </div>
-          {!isHidden && (
-            <div id="unitOptions" className="optionsDiv">
-              {unitOptions.map((option) => (
-                <div key={option} onClick={handleSelect} className="options">
-                  {option}
-                </div>
-              ))}
-            </div>
-          )}
-          <Button variant="outlined" onClick={handleDone} style={{marginTop:"20px"}}>Save</Button>
-          <Alert open={open} handleClose={handleClose}/>
-        </div>
-      </div>
-      {/* <div>
-        {selectedOptions.length > 0 && (
-          <div className="selectedDiv">
-            <div>Selected options:</div>
-            {selectedOptions.map((option) => (
-              <div key={option} onClick={() => handleRemove(option)}>
-                {option}
-              </div>
-            ))}
-          </div>
-        )}
-      </div> */}
+    <div>
+      <Select isMultiSelect={false}/>
+      <Select isMultiSelect={true}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Select.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import "./App.css";
+
+function Select({ isMultiSelect }) {
+  const [selectedOptions, setselectedOptions] = useState([]);
+  const unitOptions = ["option 1", "option 2", "option 3", "option 4", "option 5", "option 6"];
+  const [isHidden, setIsHidden] = useState(true);
+
+  const parent = (e) => {
+    setIsHidden(isHidden ? false : true);
+  }
+
+  const handleSelect = (e) => {
+    const option = e.target.textContent;
+    if (isMultiSelect) {
+      if (!selectedOptions.includes(option)) {
+        setselectedOptions([...selectedOptions, option]);
+        e.target.classList.add("selectedOption");
+      } else {
+        const updatedUnits = selectedOptions.filter(unit => unit !== option);
+        setselectedOptions(updatedUnits);
+        e.target.classList.remove("selectedOption");
+      }
+    } else {
+      setselectedOptions([option]);
+      const options = document.querySelectorAll(".options");
+      options.forEach(option => option.classList.remove("selectedOption"));
+      e.target.classList.add("selectedOption");
+      setIsHidden(true);
+    }
+  }
+  return (
+    <div className="middle">
+        <div>
+        <div className="selectDiv" onClick={parent}>
+          {selectedOptions.length > 0 ? selectedOptions.join(", ") : "Select"}
+        </div>
+        {!isHidden && (
+          <div id="unitOptions" className="optionsDiv" >
+            {unitOptions.map((option) => (
+              <div
+                key={option}
+                onClick={handleSelect}
+                className={`options ${selectedOptions.includes(option) ? "selectedOption" : ""}`}
+              >
+                <div className="opt">
+                {option}
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+    </div>
+    </div>
+  );
+}
+
+export default Select;
